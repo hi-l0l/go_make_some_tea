0b0c2ce00479f3d0ae75b1faa8a175f1c3fb1044
l0li <102256781+hi-l0l@users.noreply.github.com>
2022-04-25 20:42:01 +0300

    Update banner.py

--- banner.py
+++ banner.py
@@ -14,8 +14,33 @@ def banner():
              |       /|   /|  /| |       |  |    /|
              |---|  /-|  / | / | |-      |  |-  /-|
               ___| /  | /  |/  | |_      |  |_ /  |
+
 """
   print(banner)
 
+
+
+
+def help_banner():
+  help_banner= f"""
+{colors.purple}_____________    {colors.white} ___   ___                      _
+{colors.purple}(      {colors.blue}|%%|{colors.purple} )--  {colors.white}|     |   |     /   /|  /| | / |
+{colors.purple} (     {colors.blue}|%%|{colors.purple})   | {colors.white}|---  |   |    / | / | /-| |\  |-
+{colors.purple}  (       )----  {colors.white}|___| |___|   /  |/  |/  | | \ |_
+{colors.purple}   (_____)       {colors.white}
+              ___                 _    ____  _
+             |       /|   /|  /| |       |  |    /|
+             |---|  /-|  / | / | |-      |  |-  /-|
+              ___| /  | /  |/  | |_      |  |_ /  |
+
+-h --help           show this banner
+-o <file>           save output in file
+-p --proxychains    output format for proxychains
+"""
+  print(help_banner)
+
+
+
 if __name__=='__main__':
   banner()
+
